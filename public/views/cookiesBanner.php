5553f3649c5e1593bab37b5426e806ecc0d54a6a
PanPshemko <panpshemko@gmail.com>
2023-02-14 22:18:29 +0100

    BACKEND v0.7

---
+++ public/views/cookiesBanner.php
@@ -0,0 +1,11 @@
+            <section class="cookies-banner">
+                <div class="container">
+                    <div class="cookies-banner-text">
+                        <h1>Ta strona używa plików cookies.</h1>
+                        <p>Zamykając ten baner wyrażasz zgodę na zbieranie danych o Twoich aktywnościach prowadzonych na stronie.</p>
+                    </div>
+                    <div class="cookies-banner-button">
+                        <a href="#" class="button button-orange button2">Zamknij</a>
+                    </div>
+                </div>
+            </section>
---
+++ public/views/footer.php
@@ -0,0 +1,5 @@
+        <footer class="footer">
+            <div class="container">
+                Przemyslaw Bryla &copy; 2023
+            </div>
+        </footer>
